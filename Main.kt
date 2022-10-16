6223b1cf3daa70f98f6761613a4be417e8413a2a
Chiqooo <115097893+Chiqooo@users.noreply.github.com>
2022-10-16 12:53:38 +0400

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,55 @@
+fun main(){
+    var f1 = Fraction(1,6)
+    var f2 = Fraction (5,10)
+    println(f1.Multiply(f2))
+    println(f1.equals(f2))
+    println(f1.toString())
+    println(f2.Reduction())
+    println(f1.AddUp(f2))
+
+}
+class Fraction (val numerator:Int, val denumerator:Int){
+    fun AddUp(other: Fraction):Fraction{
+        var lcm: Int
+
+        lcm = if (denumerator > other.denumerator) denumerator else other.denumerator
+        while (true) {
+            if (lcm % denumerator == 0 && lcm % other.denumerator == 0) {
+                break
+            }
+            ++lcm
+        }
+        var newNumerator = ((lcm / denumerator) * numerator) + ((lcm / other.denumerator)) * other.numerator
+        return Fraction(newNumerator,lcm)
+    }
+    fun Multiply (other: Fraction): Fraction{
+        return Fraction (numerator * other.numerator, denumerator * other.denumerator)
+    }
+    fun Reduction (): Fraction{
+        var i = 1
+        var gcd = 1
+        //udidesi saerto gamyofi
+        while (i <= numerator && i<= denumerator){
+            if (numerator % i == 0 && denumerator % i == 0)
+                gcd = i
+            ++i
+        }
+        return Fraction(numerator / gcd,denumerator / gcd)
+    }
+
+    override fun toString(): String {
+        return "$numerator / $denumerator"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if(other is Fraction) {
+            if(numerator * other.denumerator == denumerator * other.numerator){
+                return true
+            }
+        }
+        return false
+    }
+
+}
+
+
